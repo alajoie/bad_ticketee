60feaa720969d3bf9dda5a14517531679f57f180
Alexis La Joie <alajoie@redpointtech.com>
2012-01-16 16:48:41 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BadTicketee::Application.config.secret_token = '15fe08110c06865c65348c09b47db183fdef27fa98c58b720d86858055bd688d288d388a32a4df5f2e8bdbb8e338417d615101622c99706cfd2862dee76dcfd6'
